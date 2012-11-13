d4babb90085f7784a3f27f93ceaf5d7414e1a9a1
John Murray <me@johnmurray.io>
2012-11-12 22:45:23 -0500

    Added extension to Array
    Just a couple of utility methods that make it easy to work with the
    cross-breeding code in the GA. Yes, I'm modifying the Array class
    (gasp!) but it's my personal project so get over it. lol

---
+++ runner/lib/array_ext.rb
@@ -0,0 +1,8 @@
+class Array
+  def odd_values
+    self.values_at(* self.each_index.select {|i| i.odd?})
+  end
+  def even_values
+    self.values_at(* self.each_index.select {|i| i.even?})
+  end
+end
